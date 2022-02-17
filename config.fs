f01f5802b82aea62228da10f048aa8d14ee0574d
SGCMarkus <markusornik@gmail.com>
2022-02-17 11:32:41 +0100

    paros: config.fs: Add moto GIDs

--- config.fs
+++ config.fs
@@ -28,6 +28,30 @@ value:2909
 [AID_VENDOR_QCC]
 value:2914
 
+[AID_VENDOR_BRIDGE_RW]
+value:5001
+
+[AID_VENDOR_PWRIC]
+value:5101
+
+[AID_VENDOR_TCMD]
+value:5104
+
+[AID_VENDOR_DBVC]
+value:5114
+
+[AID_VENDOR_FINGERP]
+value:5115
+
+[AID_VENDOR_DTV]
+value:5119
+
+[AID_VENDOR_MOD]
+value:5120
+
+[AID_VENDOR_PROD_PERSIST]
+value:5121
+
 [vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
