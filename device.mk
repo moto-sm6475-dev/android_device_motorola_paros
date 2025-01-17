5f78fc1244774ba8ef39320a485aec1a11e8906f
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:25:24 +0100

    paros: Build fingerprint HAL
    Change-Id: I639a283ccde7fe48dd5719adcb8350f1912a4eaa

--- device.mk
+++ device.mk
@@ -89,6 +89,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
