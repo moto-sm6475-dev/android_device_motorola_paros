1453a0cb005d5990358deff60a78a1e9ea64b4b3
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:28:23 +0100

    paros: Build QTI vibrator HAL
    Change-Id: I3d443ace9c38977829b6c47d17927bccaeb26814

--- device.mk
+++ device.mk
@@ -175,6 +175,13 @@ PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
