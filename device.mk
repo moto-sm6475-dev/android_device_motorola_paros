19a283fdeb7f087f0dfb5e25117144bdb47db42b
DY3413 <milosix3413@gmail.com>
2025-07-10 20:59:58 +0900

    paros: Import parrot overlays
    * config_wifi_dual_sap_mode_enabled does not exist in AOSP

--- device.mk
+++ device.mk
@@ -156,6 +156,12 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     Tag
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    FrameworksResTarget \
+    WifiResTarget
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
