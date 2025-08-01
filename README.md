da128ccce48b2cec766a4d326e1b84823c530860
AMWolfstein <ahmedhero4040@gmail.com>
2025-08-01 14:15:47 +0300

    paros: Initial skeleton tree

---
+++ README.md
@@ -0,0 +1,27 @@
+# Device configuration for Motorola moto g75 5G
+The moto g75 5G (codenamed _"paros"_) are Mid-range smartphones from Motorola.
+
+It was announced on 1 October 2024. Release date was 28 October 2024.
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Octa-core (4x2.4 GHz Cortex-A78 & 4x1.8 GHz Cortex-A55)
+CHIPSET | Qualcomm SM6475-AB Snapdragon 6 Gen 3 (4 nm)
+GPU     | Adreno 710
+Memory  | 8GB
+OS | Android 14, up to 5 major Android upgrades
+Storage | 128GB/265GB UFS 2.2
+Battery | 5000 mAh
+Dimensions | 166.1 x 77.2 x 8.3 mm (6.54 x 3.04 x 0.33 in)
+Display | 1080 x 2388 pixels, 6.78 IPS LCD, 120Hz, 1000 nits (HBM)
+Rear Camera 1 | 50 MP, f/1.8, (wide), 0.8µm, PDAF, OIS
+Rear Camera 2 | 8 MP, f/2.2, 119˚ (ultrawide), 1.12µm, AF
+Front Camera | 16 MP, f/2.5, (wide), 1.0µm
+Extras | NFC, Bluetooth 5.4 A2DP LE aptX HD, Fingerprint (side-mounted), USB Type-C 2.0, Fast charging 30W
+Ingress Protection | IP68, MIL-STD-810H compliant
+
+## Device picture
+
+![Device Picture](https://fdn2.gsmarena.com/vv/pics/motorola/motorola-moto-g75-1.jpg)
---
+++ extract-files.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.extract import extract_fns_user_type
+from extract_utils.extract_star import extract_star_firmware
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/motorola/paros',
+    'hardware/qcom-caf/sm8450',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    ): lib_fixup_vendor_suffix,
+    (
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+}  # fmt: skip
+
+extract_fns: extract_fns_user_type = {
+    r'(bootloader|radio)\.img': extract_star_firmware,
+}
+
+module = ExtractUtilsModule(
+    'paros',
+    'motorola',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    extract_fns=extract_fns,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
