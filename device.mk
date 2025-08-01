c9c5fc2a116ed32242346159a3e633a2bb0780e9
AMWolfstein <ahmedhero4040@gmail.com>
2025-08-01 14:40:33 +0300

    paros: Wire up recovery

--- device.mk
+++ device.mk
@@ -7,8 +7,20 @@
 BOARD_API_LEVEL := 31
 PRODUCT_SHIPPING_API_LEVEL := 34
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Init
+# Copy fstab.qcom to the vendor ramdisk first stage directory
+$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/fstab.qcom),\
+    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/$(notdir $(f))))
+
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
+
+# Recovery
+$(foreach f,$(wildcard $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc),\
+    $(eval PRODUCT_COPY_FILES += $(f):recovery/root/init/$(notdir $(f))))
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
