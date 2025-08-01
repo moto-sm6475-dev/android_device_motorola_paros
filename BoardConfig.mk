a10b8b96fc8e91dbf66e6d4d0a192a3da3f0a652
AMWolfstein <ahmedhero4040@gmail.com>
2025-08-01 14:33:32 +0300

    paros: Setup kernel args

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,44 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
 TARGET_BOOTLOADER_BOARD_NAME := paros
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 4
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_DTB_OFFSET := 0x01f00000
+BOARD_TAGS_OFFSET := 0x00000100
+
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_KERNEL_NO_GCC := true
+
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3
+
+BOARD_KERNEL_CMDLINE := \
+    androidboot.selinux=permissive \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    firmware_class.path=/data/vendor/firmware_mnt/image \
+    printk.devkmsg=on
+
+BOARD_MKBOOTIMG_ARGS += \
+    --dtb_offset $(BOARD_DTB_OFFSET) \
+    --header_version $(BOARD_BOOT_HEADER_VERSION) \
+    --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+
 # Platform
 TARGET_BOARD_PLATFORM := parrot
+
+# Recovery
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 # Inherit the proprietary files
 include vendor/motorola/paros/BoardConfigVendor.mk
